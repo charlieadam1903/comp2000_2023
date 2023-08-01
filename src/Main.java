a9b26640674519ca08648a5214b8e62f4633b85f
charlieadam1903 <charlotte.adam@students.mq.edu.au>
2023-08-01 13:52:02 +1000

    addeed JFrame + graphics imports, drawing a panel

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame {
     public static void main(String[] args) throws Exception {
-        System.out.println("Red vs. Blue");
+        Main window = new Main();
     }
+    class Canvas extends JPanel {  //jpanels an area on the screen you can draw into
+
+    }
+    private Main() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+
+    }
+    
 }
 
