9b9d9245346e041630708faa42f0497188cfc8ed
charlieadam1903 <charlotte.adam@students.mq.edu.au>
2023-08-01 15:53:47 +1000

    finally made the canvas work

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,19 @@ import java.awt.*;
 
 public class Main extends JFrame {
     public static void main(String[] args) throws Exception {
-        Main window = new Main();
+       Main window = new Main();
     }
     class Canvas extends JPanel {  //jpanels an area on the screen you can draw into
+        public Canvas() {
+            setPreferredSize(new Dimension(720, 7202));
+        }
 
     }
+    
     private Main() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Canvas canvas = new Canvas();
+        this.setContentPane(canvas);
         this.setVisible(true);
 
     }
