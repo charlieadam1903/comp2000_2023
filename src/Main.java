667a0501c00054189827dcb5a6546df7f51e0581
charlieadam1903 <charlotte.adam@students.mq.edu.au>
2023-08-02 14:06:15 +1000

    filled and painted the grid

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,30 @@ public class Main extends JFrame {
     }
     class Canvas extends JPanel {  //jpanels an area on the screen you can draw into
         public Canvas() {
-            setPreferredSize(new Dimension(720, 7202));
+            setPreferredSize(new Dimension(720, 720));
+        }
+        @Override
+        public void paint (Graphics g) {
+            for (int i = 10; i < 710; i += 35 ) {
+                for (int j = 10; j < 710; j += 35) {
+                    g.setColor(Color.WHITE);
+                    g.fillRect(i, j, 35,35);
+                    g.setColor(Color.BLACK);
+                    g.drawRect(i, j, 35,35);
+                }
+                
+            }
+           
+
         }
 
     }
-    
+
     private Main() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Canvas canvas = new Canvas();
         this.setContentPane(canvas);
+        this.pack(); //forces the size of the frame to blow up to preferred size
         this.setVisible(true);
 
     }
