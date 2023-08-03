6fdf3ac0d90aa17272e0ef20c845c8f4de55315f
charlieadam1903 <charlotte.adam@students.mq.edu.au>
2023-08-03 11:59:53 +1000

    separated grid into separate class and cell as nested class

--- src/Grid.java
+++ src/Grid.java
@@ -1,3 +1,59 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Grid {
     
-}
+        int lowerLimit;
+        int upperLimit;
+        int increment;
+        
+        class Cell {
+            int cellX;
+            int cellY;
+            Color cellFill;
+            Color cellLines;
+            int cellSize;
+           
+            public Cell(int x, int y, Color fillColor, Color linesColor, int size) {
+             this.cellX = x;
+             this.cellY = y;
+             this.cellFill = fillColor;
+             this.cellLines = linesColor;
+             this.cellSize = size;
+            }
+            public void paintCell(Graphics gr) {
+             gr.setColor(cellFill);
+             gr.fillRect(cellX,cellY, cellSize, cellSize);
+             gr.setColor(cellLines);
+             gr.drawRect(cellX, cellY, cellSize, cellSize);
+             return;
+        
+        
+           }
+        
+        
+         }
+
+        public Grid (int lower, int upper, int incr) {
+            this.lowerLimit = lower;
+            this.upperLimit = upper;
+            this.increment = incr;
+           
+
+        }
+        public void buildGrid(Graphics gr)  {
+            
+            
+            for(int i = lowerLimit; i < upperLimit; i += increment) {
+                for (int j = lowerLimit; j < upperLimit; j += increment) {
+                    Cell gridCells = new Cell(i, j, Color.WHITE, Color.BLACK, increment);
+                    gridCells.paintCell(gr);
+
+                }
+
+            }
+            return;
+
+        }
+    }
+
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import java.awt.*;
 public class Main extends JFrame {
     public static void main(String[] args) throws Exception {
        Main window = new Main();
+       return;
     }
     class Canvas extends JPanel {  //jpanels an area on the screen you can draw into
         public Canvas() {
@@ -22,66 +23,14 @@ public class Main extends JFrame {
         //         }
                 
         //     }
-        
+        return;
          }
 
     }
+   
 
-    class Cell {
-       int cellX;
-       int cellY;
-       Color cellFill;
-       Color cellLines;
-       int cellSize;
-      
-       public Cell(int x, int y, Color fillColor, Color linesColor, int size) {
-        this.cellX = x;
-        this.cellY = y;
-        this.cellFill = fillColor;
-        this.cellLines = linesColor;
-        this.cellSize = size;
-       }
-       public void paintCell(Graphics gr) {
-        gr.setColor(cellFill);
-        gr.fillRect(cellX,cellY, cellSize, cellSize);
-        gr.setColor(cellLines);
-        gr.drawRect(cellX, cellY, cellSize, cellSize);
-
-
-      }
-
-
-    }
-
-    class Grid {
-        int lowerLimit;
-        int upperLimit;
-        int increment;
-        
-        
-
-        public Grid (int lower, int upper, int incr) {
-            this.lowerLimit = lower;
-            this.upperLimit = upper;
-            this.increment = incr;
-           
-
-        }
-        public void buildGrid(Graphics gr)  {
-            
-            
-            for(int i = lowerLimit; i < upperLimit; i += increment) {
-                for (int j = lowerLimit; j < upperLimit; j += increment) {
-                    Cell gridCells = new Cell(i, j, Color.WHITE, Color.BLACK, increment);
-                    gridCells.paintCell(gr);
-
-                }
-
-            }
-
-        }
-    }
-
+    
+   
     private Main() {
         
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -96,4 +45,8 @@ public class Main extends JFrame {
     
     
 }
+
+    
+      
+
 
