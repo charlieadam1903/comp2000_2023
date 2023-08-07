f458260082ec2d948f89b9797ad0ae3a187cdbbb
charlieadam1903 <charlotte.adam@students.mq.edu.au>
2023-08-07 10:11:36 +1000

    trying to clear way for new weeks branch

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Main extends JFrame {
+    //public int cellHeightPx;
+   // public int gridSizeCells;
     public static void main(String[] args) throws Exception {
        Main window = new Main();
        return;
@@ -10,19 +12,19 @@ public class Main extends JFrame {
         public Canvas() {
             setPreferredSize(new Dimension(720, 720));
         }
-        // @Override
-         public void paint (Graphics g) {
+        @Override //override calls compiling, also sends up an error and preventing others from modifying crucial bits
+         public void paint ( Graphics g) {
             Grid canvasGrid = new Grid (10, 710, 35);
             canvasGrid.buildGrid(g);
-        //     for (int i = 10; i < 710; i += 35 ) {
-        //         for (int j = 10; j < 710; j += 35) {
-        //             g.setColor(Color.WHITE);
-        //             g.fillRect(i, j, 35,35);
-        //             g.setColor(Color.BLACK);
-        //             g.drawRect(i, j, 35,35);
-        //         }
+            for (int i = 10; i < 710; i += 35 ) {
+                for (int j = 10; j < 710; j += 35) {
+                    g.setColor(Color.WHITE);
+                    g.fillRect(i, j, 35,35);
+                    g.setColor(Color.BLACK);
+                    g.drawRect(i, j, 35,35);
+                }
                 
-        //     }
+            }
         return;
          }
 
